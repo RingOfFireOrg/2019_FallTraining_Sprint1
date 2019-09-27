d46754f1dc84dab2722d86cd495789443c484c4a
RobMackie <rob@mackies.org>
2019-09-26 21:07:34 -0400

    squaring inputs on drive train

--- src/main/java/frc/robot/TankDrive.java
+++ src/main/java/frc/robot/TankDrive.java
@@ -4,13 +4,14 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class TankDrive extends DifferentialDrive {
-	
-	
+
 	TankDrive() {
-		super(new WPI_TalonSRX(RobotMap.MOTOR_FRONT_LEFT), new WPI_TalonSRX(RobotMap.MOTOR_FRONT_RIGHT));		
+		super(new WPI_TalonSRX(RobotMap.MOTOR_FRONT_LEFT), new WPI_TalonSRX(RobotMap.MOTOR_FRONT_RIGHT));
 	}
 
 	public void drive(double leftSpeed, double rightSpeed) {
+		leftSpeed = leftSpeed * leftSpeed;
+		rightSpeed = rightSpeed * rightSpeed;
 		tankDrive(leftSpeed, rightSpeed);
-	}	
+	}
 }
